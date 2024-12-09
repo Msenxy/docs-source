d7bea95b68a5bb2e75e891b8fe46cf30373bb64a
Msenxy <msenxy@foxmail.com>
2024-12-10 00:02:29 +0800

    feat: integrate UnoCSS for styling and config setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import UnoCSS from 'unocss/vite'
+
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue(), UnoCSS(), vueDevTools()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
