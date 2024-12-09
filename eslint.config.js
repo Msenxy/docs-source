b0245d0a41a496d644104549919a44a60e46bf22
Msenxy <msenxy@foxmail.com>
2024-12-09 09:41:06 +0800

    refactor: format eslint.config.js for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -4,18 +4,19 @@ import oxlint from 'eslint-plugin-oxlint'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}']
+    },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+    },
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  oxlint.configs['flat/recommended'],
-  skipFormatting,
+    ...pluginVue.configs['flat/essential'],
+
+    ...vueTsEslintConfig(),
+    oxlint.configs['flat/recommended'],
+    skipFormatting
 ]
