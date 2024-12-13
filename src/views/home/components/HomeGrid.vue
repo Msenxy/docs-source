a0175f4bc979868f3b7363ca8dda26647d74f6ba
Msenxy <msenxy@foxmail.com>
2024-12-13 13:17:58 +0800

    refactor: simplify HomeGrid icons with dynamic list

--- src/views/home/components/HomeGrid.vue
+++ src/views/home/components/HomeGrid.vue
@@ -1,54 +1,51 @@
 <template>
     <div class="icon-container" :class="{ 'is-visible': isLogoClicked }">
         <Icon
-            icon="mdi:github"
+            v-for="(item, index) in gridList"
+            :key="index"
+            :icon="item.icon"
             class="social-icon"
-            @click="handleIconClick('github')"
-        />
-        <Icon
-            icon="mdi:twitter"
-            class="social-icon"
-            @click="handleIconClick('twitter')"
-        />
-        <Icon
-            icon="mdi:instagram"
-            class="social-icon"
-            @click="handleIconClick('instagram')"
-        />
-        <Icon
-            icon="mdi:email"
-            class="social-icon"
-            @click="handleIconClick('email')"
-        />
-        <Icon
-            icon="mdi:more-horiz"
-            class="social-icon"
-            @click="handleIconClick('more')"
+            @click="item.click"
         />
     </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
     import router from '@/router'
     import { Icon } from '@iconify/vue'
 
     defineProps<{ isLogoClicked: boolean }>()
 
-    const handleIconClick = (type: string) => {
-        if (type === 'more') {
-            router.push('/docs')
-            return
-        }
-
-        const links = {
-            github: 'https://github.com',
-            twitter: 'https://twitter.com',
-            instagram: 'https://instagram.com',
-            email: 'mailto:your-email@example.com'
-        }
-
-        window.open(links[type as keyof typeof links], '_blank')
-    }
+    const gridList = [
+        { icon: 'mdi:more-horiz', click: () => router.push('/docs') }
+    ]
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .icon-container {
+        display: flex;
+        gap: 2rem;
+        position: absolute;
+        bottom: 15%;
+        opacity: 0;
+        transition: opacity 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+
+        &.is-visible {
+            opacity: 1;
+        }
+    }
+
+    .social-icon {
+        font-size: 2rem;
+        color: oklch(32.11% 0 0);
+        cursor: pointer;
+        transition:
+            transform 0.3s ease,
+            color 0.3s ease;
+
+        &:hover {
+            transform: scale(1.2);
+            color: oklch(51.03% 0 0);
+        }
+    }
+</style>
