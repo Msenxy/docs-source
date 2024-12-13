1eab296ecd0548cea9b42b6737593a2e588ea391
Msenxy <msenxy@foxmail.com>
2024-12-13 13:17:52 +0800

    feat: add HomeMenu and style enhancements to home view

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,38 @@
 <template>
-    <HomeLogo v-model="isLogoClicked" />
-    <HomeGrid :isLogoClicked />
+    <div class="home">
+        <HomeMenu />
+        <HomeLogo v-model="isLogoClicked" />
+        <HomeGrid :isLogoClicked />
+    </div>
 </template>
 
 <script lang="ts" setup>
-    import { ref, watch } from 'vue'
+    import { ref, watch, unref } from 'vue'
 
-    import HomeGrid from '@/views/home/components/HomeGrid.vue'
+    import HomeMenu from '@/views/home/components/HomeMenu.vue'
     import HomeLogo from '@/views/home/components/HomeLogo.vue'
-
+    import HomeGrid from '@/views/home/components/HomeGrid.vue'
     import { useLocalStorage } from '@/hooks/useLocalStorage'
 
     const simpleLayout = useLocalStorage('simpleLayout', false)
 
-    const isLogoClicked = ref(false)
+    const isLogoClicked = ref(unref(simpleLayout))
 
     watch(simpleLayout, newVal => {
         isLogoClicked.value = newVal
     })
 </script>
+
+<style scoped>
+    .home {
+        height: 100vh;
+        width: 100vw;
+        background: linear-gradient(oklch(87% 0.01 258), oklch(98% 0.02 74));
+        display: flex;
+        flex-direction: column;
+        gap: 40px;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
