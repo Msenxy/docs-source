60133854d0105316036514fc5b180c130b8d764c
Msenxy <msenxy@foxmail.com>
2024-12-16 06:19:32 +0800

    feat: add typed-router for auto routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/home/index.vue'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,8 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
-        }
+        },
+        ...routes
     ]
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import Markdown from 'unplugin-vue-markdown/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+
+import { markdownHeadingPlugin } from './src/plugin/markdown-heading'
+
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+        VueRouter({
+            extensions: ['.vue', '.md'],
+            routesFolder: 'src/pages'
+        }),
+
+        vue({ include: [/\.vue$/, /\.md$/] }),
+
+        Markdown({
+            markdownItSetup(md) {
+                md.use(markdownHeadingPlugin)
+            }
+        }),
+
+        vueDevTools()
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
