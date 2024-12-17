08c914ec9fd1713d9ab6e281460393ab60e204ac
Msenxy <msenxy@foxmail.com>
2024-12-18 07:47:07 +0800

    feat: add filename plugin and update heading plugin in Markdown

--- src/plugin/markdown-heading.ts
+++ src/plugin/markdown-heading.ts
@@ -1,13 +1,12 @@
-// src/plugins/markdown-heading.ts
 import type MarkdownIt from 'markdown-it'
 import Token from 'markdown-it/lib/token.mjs'
 
-export function markdownHeadingPlugin(md: MarkdownIt) {
-    const handleHeading = (token: Token) => {
-        const level = parseInt(token.tag.slice(1))
-        return level <= 3 ? `h${level + 1}` : 'p'
-    }
+const handleHeading = (token: Token) => {
+    const level = parseInt(token.tag.slice(1))
+    return level <= 3 ? `h${level + 1}` : 'p'
+}
 
+export const heading = (md: MarkdownIt) => {
     md.renderer.rules.heading_open = (tokens, idx) =>
         `<${handleHeading(tokens[idx])}>`
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import Markdown from 'unplugin-vue-markdown/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 
-import { markdownHeadingPlugin } from './src/plugin/markdown-heading'
+import { heading } from './src/plugin/markdown-heading'
+import { filename } from './src/plugin/markdown-filename'
 
 export default defineConfig({
     plugins: [
@@ -20,7 +21,13 @@ export default defineConfig({
 
         Markdown({
             markdownItSetup(md) {
-                md.use(markdownHeadingPlugin)
+                md.use(heading)
+                md.use(filename)
+            },
+            transforms: {
+                before(code) {
+                    return code
+                }
             }
         }),
 
