8029ac7d1e122b46bafc7623cd68c1de4f07d142
Msenxy <msenxy@foxmail.com>
2024-12-22 12:13:37 +0800

    feat: 在 Vite 配置中添加 Shikiji 和相关插件以增强 Markdown 支持

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import Markdown from 'unplugin-vue-markdown/vite'
+import Shikiji from '@shikijs/markdown-it'
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import { heading } from './src/plugin/markdown-heading'
 import { filename } from './src/plugin/markdown-filename'
+import { space } from './src/plugin/markdown-space'
 
 export default defineConfig({
     plugins: [
@@ -20,9 +23,25 @@ export default defineConfig({
         vue({ include: [/\.vue$/, /\.md$/] }),
 
         Markdown({
-            markdownItSetup(md) {
+            async markdownItSetup(md) {
+                md.use(
+                    await Shikiji({
+                        themes: {
+                            light: 'vitesse-light',
+                            dark: 'vitesse-dark'
+                        },
+                        transformers: [
+                            transformerTwoslash({
+                                explicitTrigger: true,
+                                renderer: rendererRich()
+                            })
+                        ]
+                    })
+                )
+
                 md.use(heading)
                 md.use(filename)
+                md.use(space)
             },
             transforms: {
                 before(code) {
