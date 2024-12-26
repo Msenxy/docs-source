618fc9c8205c6bad93138a96518fe3b6b19c88c2
Msenxy <msenxy@foxmail.com>
2024-12-27 02:37:41 +0800

    feat: 添加目录视图组件以动态展示路由项

---
+++ src/views/directory/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-for="(item, index) in routes" :key="index">
+        {{ item }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { routes } from 'vue-router/auto-routes'
+</script>
+
+<style lang="scss" scoped></style>
