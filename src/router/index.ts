8202ae06ddddec12562ee25c8d9e9b8e5d93f5f5
Msenxy <msenxy@foxmail.com>
2024-12-27 02:36:24 +0800

    feat: 添加目录路由并处理路径解码

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/home/index.vue'
 import { routes } from 'vue-router/auto-routes'
 
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
@@ -10,6 +10,19 @@ export default createRouter({
             name: 'home',
             component: HomeView
         },
+        {
+            path: '/directory',
+            name: 'directory',
+            component: () => import('@/views/directory/index.vue')
+        },
         ...routes
     ]
 })
+
+router.beforeEach((to, _from, next) =>
+    to.path !== decodeURIComponent(to.path) ?
+        next({ path: decodeURIComponent(to.path), replace: true })
+    :   next()
+)
+
+export default router
