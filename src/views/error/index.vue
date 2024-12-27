956b53fdff03efe976e293c9d17c908d59b67052
Msenxy <msenxy@foxmail.com>
2024-12-27 21:07:09 +0800

    feat: 添加404错误页面以处理未找到的路由

---
+++ src/views/error/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="not-found">
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/">Go back to Home</router-link>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+    .not-found {
+        text-align: center;
+        margin-top: 50px;
+    }
+</style>
