cb75b1c546c4428945b0d63114dfb6aad9920f9e
Msenxy <msenxy@foxmail.com>
2025-03-20 01:38:32 +0800

    feat: 在主应用中注册 MarkdownWrapper 组件

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { ViteSSG } from 'vite-ssg'
 import App from '@/App.vue'
 import routes from '@/router'
 
+import MarkdownWrapper from '@/views/docs/index.vue'
+
 import '@/styles/main.scss'
 import '@/styles/markdown.scss'
 import '@shikijs/twoslash/style-rich.css'
 
-export const createApp = ViteSSG(App, { routes }, ({ router }) => {
+export const createApp = ViteSSG(App, { routes }, ({ app, router }) => {
+    app.component('MarkdownWrapper', MarkdownWrapper)
+
     router.beforeEach((to, _from, next) =>
         to.path !== decodeURIComponent(to.path) ?
             next({ path: decodeURIComponent(to.path), replace: true })
